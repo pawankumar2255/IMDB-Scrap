959d7e9b6c222970a5dc4cf3d3f140c63ff67834
pawankumar2255 <82705703+pawankumar2255@users.noreply.github.com>
2021-07-21 19:29:21 +0530

    first commit

--- imdb6.py
+++ imdb6.py
@@ -1,1 +1,15 @@
-
+import json 
+m_f = open("task4s.json","r")
+movie_list = json.load(m_f)
+langs_list,set1,dict_ana_lang = [],set(),{}
+def Analysis_movie_language():
+    for movie_det in movie_list:
+        for language in movie_det["language"]:
+            langs_list.append(language)
+            set1.add(language)
+    for langs in set1:
+        dict_ana_lang[langs]=langs_list.count(langs)
+    file = open("task6s.json","w")
+    json.dump(dict_ana_lang,file,indent = 4)
+    file.close()
+Analysis_movie_language()
